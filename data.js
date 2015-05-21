b1d83d61d5b1b84a0b264e98f54c56dd6ba8d98d
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-21 10:56:43 -0700

    templating practice

---
+++ data.js
@@ -0,0 +1,30 @@
+var loons = [
+  {
+    name: "Bugs",
+    type: "Bunny",
+    strength: "Wit",
+    weakness: "Hunting season",
+    pic: "http://vignette1.wikia.nocookie.net/characters/images/1/15/Bugs.jpg/revision/latest?cb=20110224072532"
+  },
+  {
+    name: "Porky",
+    type: "Pig",
+    strength: "Charm",
+    weakness: "Stuttering",
+    pic: "https://s3.amazonaws.com/live_object_images/3719/1.jpeg"
+  },
+  {
+    name: "Daffy",
+    type: "Duck",
+    strength: "Regeneration",
+    weakness: "Shotguns",
+    pic: "http://vignette1.wikia.nocookie.net/looneytunes/images/4/42/DaffyDuck.jpg/revision/latest?cb=20110425123102"
+  },
+  {
+    name: "Taz",
+    type: "Tazmanian Devil",
+    strength: "Spinning & eating",
+    weakness: "Anger management classes",
+    pic: "http://i12.photobucket.com/albums/a210/JRuff/Art/Improved%20versions/taz-clenshing_fist-photoshopped.png"
+  }
+]
